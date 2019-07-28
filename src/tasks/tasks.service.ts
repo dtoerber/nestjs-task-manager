1f2b69e9903ec2595102d2001e520c008f9e1b9e
Dave Toerber <d.toerber@marrickdevelopment.com>
2019-07-28 16:25:16 -0600

    feature throw custom not found error

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskPutDTO } from './dto/create-task.put.dto';
 import { GetTaskFilterDTO } from './dto/get-tasks-filter.dto';
@@ -44,7 +44,12 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id === id);
+    const found = this.tasks.find(task => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Task ID=${id} not found.`);
+    }
+    return found;
   }
 
   deleteTaskById(id: string): void {
